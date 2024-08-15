705e1b3a506d961a3e7e0bf2765682ad96b674c2
qdinh <dqdung999@gmail.com>
2024-08-15 21:48:54 +0200

    Commit

--- coco2voc.sh
+++
@@ -1,3 +0,0 @@
-python ./dataset/coco2VOC/coco2voc.py  \
-                    --ann_file "/home/qdinh/data/DOTA/annotations/instances_train2017unseen.json" \
-                    --output_dir "/home/qdinh/data/DOTA/annotations_VOCstyle/train2017unseen"
